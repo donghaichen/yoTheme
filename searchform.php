fa2fa0d703751fa7a31d6ca829091f796a574fcf
donghaichen <384560806@qq.com>
2015-03-27 21:35:15 +0800

    yoTheme 的第三个版本
    yoTheme 的第三个版本

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <input type="text" name="s" id="s" class="sk" />
+    <input type="submit" name="submit" id="searchsubmit" class="sb" />
+</form>
